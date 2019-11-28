247f0922dbc762593dc04c13d7d7e03cae32d0e5
gongmeng <18588264128@163.com>
2019-11-28 06:33:22 +0000

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\User;
 
 class UsersController extends Controller
@@ -24,6 +23,17 @@ class UsersController extends Controller
     	//dump(compact('user'));
 
         return view('users.show', compact('user'));
+    }
+
+    //处理创建请求
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 
 }
